5d948b2e5f8103cfb718557b50892cdb26e48a2b
Vladislav-Proshchenkov <vladislove.pro@gmail.com>
2025-06-01 19:41:12 +0300

    Initial gh-pages commit

---
+++ 127ae8224072d3826a4d.js
@@ -0,0 +1,1 @@
+import"./css/styles.css";import HelpDesk from"./js/HelpDesk";import TicketService from"./js/TicketService";const root=document.getElementById("root"),ticketService=new TicketService,app=new HelpDesk(root,ticketService);app.init();
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './css/styles.css';
-import HelpDesk from './js/HelpDesk';
-import TicketService from './js/TicketService';
-
-const root = document.getElementById('root');
-const ticketService = new TicketService();
-const app = new HelpDesk(root, ticketService);
-
-app.init();
